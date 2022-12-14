e40259c5dd5bc8aaf165742aa772b9259a62f9d5
francopoffo <franco_sutter@yahoo.com.br>
2022-12-14 11:11:34 -0300

    Criando a lógica da conversão

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import CurrencyModule from "./CurrencyModule";
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([]);
-  
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+
   const URL = "https://api.apilayer.com/exchangerates_data/latest";
+ 
 
   var myHeaders = new Headers();
   myHeaders.append("apikey", "nS9xRtJMdd55lWaOpkQdDC3uVKjDS1Mt");
@@ -19,18 +34,60 @@ function App() {
   useEffect(() => {
     fetch(URL, requestOptions)
       .then((res) => res.json())
-      .then((data) => setCurrencyOptions([Object.keys(data.rates)]))
+      .then((data) => {
+        const firstCurrency = Object.keys(data.rates)[0];
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
+        setExchangeRate(data.rates[firstCurrency]);
+      })
       .catch((error) => console.log("error", error));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[]);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(
+        `https://api.apilayer.com/exchangerates_data/latest?symbols=${toCurrency}&base=${fromCurrency}`,
+        requestOptions
+      )
+        .then((res) => res.json())
+        .then((data) => setExchangeRate(data.rates[toCurrency]))
+        .catch((error) => console.log("error", error));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toCurrency, fromCurrency]);
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  }
 
   return (
     <Fragment>
-      <h1>Conversor monetário</h1>
       <div className="container">
-        <CurrencyModule currencyOptions={currencyOptions}/>
+        <h1>Conversor monetário</h1>
+        <CurrencyModule
+          currencyOptions={currencyOptions}
+          selectedCurrency={fromCurrency}
+          onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+          onChangeAmount={handleFromAmountChange}
+          amount={fromAmount}
+        />
         <div className="igual">=</div>
-        <CurrencyModule currencyOptions={currencyOptions}/>
+        <CurrencyModule
+          currencyOptions={currencyOptions}
+          selectedCurrency={toCurrency}
+          onChangeCurrency={(e) => setToCurrency(e.target.value)}
+          onChangeAmount={handleToAmountChange}
+          amount={toAmount}
+        />
       </div>
     </Fragment>
   );
--- src/CurrencyModule.js
+++ src/CurrencyModule.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 export default function CurrencyModule(props) {
-    const { currencyOptions } = props;
+    const { 
+      currencyOptions,
+      selectedCurrency,
+      onChangeCurrency,
+      onChangeAmount,
+      amount
+
+    } = props;
 
   return (
     <div>
-      <input type="number"></input>
-      <select>
+      <input type="number" value={amount} onChange={onChangeAmount}></input>
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
         {currencyOptions.map(option =>(
             <option key={option} value={option}>{option}</option>
         ))}
